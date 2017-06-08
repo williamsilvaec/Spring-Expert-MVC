8cd8d5b752cff244abf10173624bcc68d189e1d5
williamsilvaec <williamsousadasilva@gmail.com>
2017-06-07 23:57:48 -0300

    Introdução a validação do formulário

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -2,6 +2,9 @@ package com.algaworks.brewer.controller;
 
 import com.algaworks.brewer.model.Cerveja;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,7 +21,11 @@ public class CervejasController {
     }
 
     @RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-    public String cadastrar(Cerveja cerveja) {
+    public String cadastrar(@Validated Cerveja cerveja, BindingResult result) {
+
+        if (result.hasErrors()) {
+            System.out.println(">>>> tem erro!");
+        }
 
         System.out.println("sku >>>>>>" + cerveja.getSku());
 
--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -1,11 +1,15 @@
 package com.algaworks.brewer.model;
 
 
+import org.hibernate.validator.constraints.NotBlank;
+
+
 /**
  * Created by willi on 07/06/2017.
  */
 public class Cerveja {
 
+    @NotBlank
     private String sku;
     private String nome;
 
