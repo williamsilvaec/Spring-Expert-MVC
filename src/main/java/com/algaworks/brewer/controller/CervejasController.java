61207decd1af9b3457399f5835d6bf8d05b6dde3
williamsilvaec <williamsousadasilva@gmail.com>
2017-06-08 00:27:38 -0300

    Forward e Redirect

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -2,11 +2,12 @@ package com.algaworks.brewer.controller;
 
 import com.algaworks.brewer.model.Cerveja;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * Created by willi on 06/06/2017.
@@ -21,14 +22,16 @@ public class CervejasController {
     }
 
     @RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-    public String cadastrar(@Validated Cerveja cerveja, BindingResult result) {
+    public String cadastrar(@Validated Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 
         if (result.hasErrors()) {
-            System.out.println(">>>> tem erro!");
+            model.addAttribute("mensagem", "Erro no formulário!");
+            return "cerveja/CadastroCerveja";
         }
 
         System.out.println("sku >>>>>>" + cerveja.getSku());
+        attributes.addFlashAttribute("mensagem", "salvo com sucesso!");
 
-        return "cerveja/CadastroCerveja";
+        return "redirect:/cervejas/novo";
     }
 }
