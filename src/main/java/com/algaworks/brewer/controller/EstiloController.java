f744f9c639425be5f89e7e3ec8a1efffc1ef62b7
williamsilvaec <williamsilvaec@gmail.com>
2017-08-13 22:36:37 -0300

    Tratando exceções com ExceptionHandler

--- src/main/java/com/algaworks/brewer/controller/EstiloController.java
+++ src/main/java/com/algaworks/brewer/controller/EstiloController.java
@@ -55,11 +55,7 @@ public class EstiloController {
             return ResponseEntity.badRequest().body(result.getFieldError("nome").getDefaultMessage());
         }
 
-        try {
-            estilo = cadastroEstiloService.salvar(estilo);
-        } catch (NomeEstiloJaCadastradoException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        estilo = cadastroEstiloService.salvar(estilo);
 
         return ResponseEntity.ok(estilo);
     }
---
+++ src/main/java/com/algaworks/brewer/controller/handler/ControllerAdviceExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.algaworks.brewer.controller.handler;
+
+import com.algaworks.brewer.service.exception.NomeEstiloJaCadastradoException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerAdviceExceptionHandler {
+
+    @ExceptionHandler(NomeEstiloJaCadastradoException.class)
+    public ResponseEntity<String> handleNomeEstiloJaCadastradoException(NomeEstiloJaCadastradoException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
