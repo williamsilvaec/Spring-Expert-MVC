6c6d11b38e813b564e41a0ca09d25463e7550a69
williamsilvaec <williamsilvaec@gmail.com>
2017-08-16 22:59:53 -0300

    Prototipando a pesquisa da cerveja

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -3,6 +3,7 @@ package com.algaworks.brewer.controller;
 import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.model.Origem;
 import com.algaworks.brewer.model.Sabor;
+import com.algaworks.brewer.repository.Cervejas;
 import com.algaworks.brewer.repository.Estilos;
 import com.algaworks.brewer.service.CadastroCervejaService;
 import org.slf4j.Logger;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,15 +23,18 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
  * Created by willi on 06/06/2017.
  */
 @Controller
+@RequestMapping("/cervejas")
 public class CervejasController {
 
     private static final Logger logger = LoggerFactory.getLogger(CervejasController.class);
 
     @Autowired private Estilos estilos;
 
+    @Autowired private Cervejas cervejas;
+
     @Autowired private CadastroCervejaService cadastroCervejaService;
 
-    @RequestMapping("/cervejas/novo")
+    @RequestMapping("/novo")
     public ModelAndView novo(Cerveja cerveja) {
         ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
         mv.addObject("sabores", Sabor.values());
@@ -38,7 +43,7 @@ public class CervejasController {
         return mv;
     }
 
-    @RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+    @RequestMapping(value = "/novo", method = RequestMethod.POST)
     public ModelAndView cadastrar(@Validated Cerveja cerveja, BindingResult result,
                                   Model model, RedirectAttributes attributes) {
 
@@ -49,6 +54,16 @@ public class CervejasController {
         cadastroCervejaService.salvar(cerveja);
         attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
         return new ModelAndView("redirect:/cervejas/novo");
+    }
+
+    @GetMapping
+    public ModelAndView pesquisar() {
+        ModelAndView mv = new ModelAndView("/cerveja/PesquisaCervejas");
+        mv.addObject("sabores", Sabor.values());
+        mv.addObject("estilos", estilos.findAll());
+        mv.addObject("origens", Origem.values());
+        mv.addObject("cervejas", cervejas.findAll());
+        return mv;
     }
 
     @RequestMapping(value = "/cadastro/novo")
