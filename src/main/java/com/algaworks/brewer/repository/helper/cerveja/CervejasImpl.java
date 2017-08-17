3eef6b62b2af21fea13f4c7ca1b29ca04ef2ffec
william <williamsousadasilva@gmail.com>
2017-08-17 11:30:35 -0300

    Prototipando a paginação

--- src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasImpl.java
+++ src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasImpl.java
@@ -1,7 +1,6 @@
 package com.algaworks.brewer.repository.helper.cerveja;
 
 import com.algaworks.brewer.model.Cerveja;
-import com.algaworks.brewer.model.Estilo;
 import com.algaworks.brewer.repository.filter.CervejaFilter;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
