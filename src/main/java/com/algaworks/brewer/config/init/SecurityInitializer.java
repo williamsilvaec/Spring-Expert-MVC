e737a4ba66834751c92db8866971dc8c40c7f95c
williamsilvaec <williamsilvaec@gmail.com>
2017-09-11 23:41:53 -0300

    Resolvendo problema da acentuação após adicionar o spring security.

--- src/main/java/com/algaworks/brewer/config/init/SecurityInitializer.java
+++ src/main/java/com/algaworks/brewer/config/init/SecurityInitializer.java
@@ -1,6 +1,19 @@
 package com.algaworks.brewer.config.init;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
 
 public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+    @Override
+    protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+        FilterRegistration.Dynamic characterEncodingFilter = servletContext.addFilter("encodingFilter",
+                new CharacterEncodingFilter());
+        characterEncodingFilter.setInitParameter("encoding", "UTF-8");
+        characterEncodingFilter.setInitParameter("forceEncoding", "true");
+        characterEncodingFilter.addMappingForUrlPatterns(null, false, "/*");
+    }
 }
