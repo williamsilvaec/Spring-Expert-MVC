e6619351de3135c2e926b43285ebe60825909814
williamsilvaec <williamsilvaec@gmail.com>
2017-09-11 23:50:28 -0300

    Expirando sessão com novo login.

--- src/main/java/com/algaworks/brewer/config/SecurityConfig.java
+++ src/main/java/com/algaworks/brewer/config/SecurityConfig.java
@@ -48,7 +48,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                     .and()
                 .exceptionHandling()
-                    .accessDeniedPage("/403");
+                    .accessDeniedPage("/403")
+                    .and()
+                .sessionManagement()
+                    .maximumSessions(1)
+                    .expiredUrl("/login");
     }
 
     @Bean
