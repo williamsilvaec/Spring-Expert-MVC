a5dbdc763631dea3c6749fe55142d483507bd349
william.silva <william@viasoft.com.br>
2018-02-09 16:58:54 -0200

    Duração da sessão

--- src/main/java/com/algaworks/brewer/config/init/SecurityInitializer.java
+++ src/main/java/com/algaworks/brewer/config/init/SecurityInitializer.java
@@ -5,11 +5,17 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
+import javax.servlet.SessionTrackingMode;
+import java.util.EnumSet;
 
 public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 
     @Override
     protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+
+        // para o jsessionid ir para o cookie
+        servletContext.setSessionTrackingModes(EnumSet.of(SessionTrackingMode.COOKIE));
+
         FilterRegistration.Dynamic characterEncodingFilter = servletContext.addFilter("encodingFilter",
                 new CharacterEncodingFilter());
         characterEncodingFilter.setInitParameter("encoding", "UTF-8");
